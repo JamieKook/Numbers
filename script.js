2cf4e69933937637f8d20f43deffa0d4b7d07bdc
Jamie <kookjc6@gmail.com>
2019-12-14 21:37:27 -0600

    DRY slideBead function

--- script.js
+++ script.js
@@ -9,13 +9,7 @@ function findBead(status){
         spot=beadCount; 
         isDoneFunction=true; 
         return spot;
-    }
-    // if (status==="right"){
-    //     beadCount++; 
-    // } else if (status === "left"){
-    //     beadCount--; 
-    // }
-    
+    }   
 }
 
 function beadChecker(status, direction){
@@ -37,77 +31,60 @@ function beadChecker(status, direction){
     return spot; 
 }
 
+function moveBead(direction) {
+    let oppDirection= null; 
+    if (direction === "right"){
+        oppDirection= "left"; 
+    } else if (direction === "left"){
+        oppDirection= "right"; 
+    }
+    canClick=false; 
+    moveSpot= beadChecker("false", direction); 
+    x= $("#spot"+moveSpot).offset().left;  
+    let counter= $("#spot"+beadSpot).find(".counter");
+    var xi = $(counter).offset().left;
+    $(counter).css("position", "absolute"); 
+    $(counter).animate({
+        left: (x-xi),
+    });
+    $("#spot"+moveSpot).attr("data-counter", oppDirection);
+    $("#spot"+beadSpot).attr("data-counter", "false");
+    function wait(){
+        let timer= setInterval(function(){
+            let time=0; 
+            if (time < 1) {
+                clearInterval(timer);
+                $("#spot"+beadSpot).html(""); 
+                let beadImageHtml= `<img class="counter img-fluid" src="images/counter.jpeg">`;
+                $("#spot"+moveSpot).html(beadImageHtml);
+                $("#spot"+moveSpot).find(".counter").on("click", function(){
+                    let beadEl = $(this).parent().attr("id"); 
+                    beadSpot= parseInt(beadEl.split("ot")[1]); 
+                    slideBead(beadSpot); 
+                }); 
+                canClick=true;  
+            }
+            time++;
+        }, 500)
+    }
+    wait(); 
+}
+
+
 function slideBead(beadSpot){
     debugger; 
     let x;
     spot = beadChecker("right", "right");
     if (beadSpot === spot && canClick){
-        canClick=false; 
-        moveSpot= beadChecker("false", "right"); 
-        x= $("#spot"+moveSpot).offset().left;  
-        let counter= $("#spot"+beadSpot).find(".counter");
-        var xi = $(counter).offset().left;
-        $(counter).css("position", "absolute"); 
-        $(counter).animate({
-            left: (x-xi),
-        });
-        $("#spot"+moveSpot).attr("data-counter", "left");
-        $("#spot"+beadSpot).attr("data-counter", "false");
-        function wait(){
-            let timer= setInterval(function(){
-               let time=0; 
-               if (time < 1) {
-                    clearInterval(timer);
-                    $("#spot"+beadSpot).html(""); 
-                    let beadImageHtml= `<img class="counter img-fluid" src="images/counter.jpeg">`;
-                    $("#spot"+moveSpot).html(beadImageHtml);
-                    $("#spot"+moveSpot).find(".counter").on("click", function(){
-                        let beadEl = $(this).parent().attr("id"); 
-                        beadSpot= parseInt(beadEl.split("ot")[1]); 
-                        slideBead(beadSpot); 
-                    }); 
-                    canClick=true;  
-                }
-                time++;
-            }, 500)
-        }
-        wait();  
+       moveBead("right");  
     } else {
         spot= beadChecker("left", "left"); 
         if (beadSpot === spot && canClick){
-            canClick=false; 
-            moveSpot= beadChecker("false", "left"); 
-            x= $("#spot"+moveSpot).offset().left;  
-            let counter= $("#spot"+beadSpot).find(".counter");
-            var xi = $(counter).offset().left;
-            $(counter).css("position", "absolute"); 
-            $(counter).animate({
-                left: (x-xi),
-            });
-            $("#spot"+moveSpot).attr("data-counter", "right");
-            $("#spot"+beadSpot).attr("data-counter", "false");
-            function wait(){
-                let timer= setInterval(function(){
-                   let time=0; 
-                   if (time < 1) {
-                        clearInterval(timer);
-                        $("#spot"+beadSpot).html(""); 
-                        let beadImageHtml= `<img class="counter img-fluid" src="images/counter.jpeg">`;
-                        $("#spot"+moveSpot).html(beadImageHtml);
-                        $("#spot"+moveSpot).find(".counter").on("click", function(){
-                            let beadEl = $(this).parent().attr("id"); 
-                            beadSpot= parseInt(beadEl.split("ot")[1]); 
-                            slideBead(beadSpot); 
-                        }); 
-                        canClick=true;  
-                    }
-                    time++;
-                }, 500)
-            }
-            wait(); 
+           moveBead("left"); 
         }
     }
 }
+
 
 $(".counter").on("click", function(){
     let beadEl = $(this).parent().attr("id"); 
